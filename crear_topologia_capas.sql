66eaa74ebb39a4de3b68e1df08d5d968bbb0c3f7
Roberto J <programapa.contacta@gmail.com>
2021-04-12 18:28:19 +0200

    Create crear_topologia_capas.sql

---
+++ crear_topologia_capas.sql
@@ -0,0 +1,22 @@
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_topology;
+
+-- CREAR ESQUEMA TOPOLOGICO
+
+SELECT topology.CreateTopology('nombre_topología', SRC, tolerancia);
+
+-- AÑADIR COLUMNA TOPOGEOMETRICA
+
+SELECT topology.AddTopoGeometryColumn('nombre_topo', 'esquema_capa', 'nombre_capa', 'nombre_topogeom', 'tipo_geometría');
+
+-- CREAR TOPOLOGIA
+
+UPDATE nombre_capa 
+SET topogeom = topology.toTopoGeom(
+     columna_geom, 'esquema_topo', 
+     layer_id, tolerancia
+);
+
+-- BORRAR TOPOLOGIA
+
+SELECT topology.DropTopology('nombre_capa');
