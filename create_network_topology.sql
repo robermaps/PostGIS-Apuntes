398ec606be53af6894d1b1896fc14a86c1432601
Roberto J <programapa.contacta@gmail.com>
2021-11-03 12:44:52 +0100

    trad

---
+++ create_network_topology.sql
@@ -0,0 +1,14 @@
+-- ADD EXTENSIONS
+
+create extension postgis;
+create extension pgrouting;
+
+-- CREATE COLUMNS TO TARGET TABLE (LAYER)
+
+ALTER TABLE table_name ADD COLUMN cost double precision;
+ALTER TABLE table_name ADD COLUMN source int;
+ALTER TABLE table_name ADD COLUMN target int;
+
+-- CREAR TOPOLOGIA
+
+SELECT pgr_createTopology('table_name', tolerance, 'geometry_column', 'gid', 'source', 'target');
